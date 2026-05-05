f2583d484536b00f44aa1b2bdf7d7cc50586f428
neeil1990 <neeil@mail.ru>
2026-05-05 17:14:11 +0300

    Fix case collision in file names

--- bitrix/modules/sotbit.seometa/lang/ru/lib/controllers/excelExportImport.php
+++
@@ -1,6 +0,0 @@
-<?php
-$MESS["SEO_META_CHPU_RUN_INIT"] = "Выполнено...";
-$MESS["SEO_META_CHPU_RUN_TITLE"] = "Выгрузка ЧПУ";
-$MESS["SEO_META_COND_RUN_INIT"] = "Выполнено...";
-$MESS["SEO_META_COND_RUN_TITLE"] = "Выгрузка условий";
-$MESS["SEO_META_RUN_FINISH"] = "Успешно выгружено";
